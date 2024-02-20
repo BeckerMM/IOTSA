11ae9eab8a12f0af868114fb63637e2cd0b9189c
Matheus Becker <beckerm@weg.net>
2024-02-20 13:56:18 -0300

    first commit

---
+++ src/components/Display/Display.tsx
@@ -0,0 +1,11 @@
+export const Display =() => {
+  return (
+    <>
+    
+    <div className="h-[30%] w-[30%] bg-blue-200 rounded-sm">
+        
+    </div>
+    
+    </>
+  );
+}
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+    return (
+     <div className=" h-[8%] w-full bg-blue-400">
+
+     </div>  
+    )
+}
