49ecbc7ab06f21651cae6ba2e6648786a788ca7a
Gabriel Fellipe Marquardt <gfellipe@weg.net>
2024-02-23 18:37:15 -0300

    a

--- lacticio/src/main/java/com/laticinios/laticinio/repository/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.laticinios.laticinio.repository;
-
-import com.laticinios.laticinio.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
-}
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,12 @@
 export const Header = () => {
     return (
-     <div className=" h-[8%] w-full bg-blue-400">
-
-     </div>  
+        <div className=" h-[8%] w-full bg-blue-400 flex justify-around items-center">
+            <div>
+                <a className="font-semibold text-2xl" href="/">Página inicial</a>
+            </div>
+            <div>
+                <a className="font-semibold text-2xl" href="http://localhost:3000/delivery-list">Histórico de entregas</a>
+            </div>
+        </div>
     )
 }
